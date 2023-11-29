d690f9d178432b216fea1ab646abd85cd71bc66d
Anas Gouda <anas.gouda@tu-dortmund.de>
2023-11-29 15:50:07 +0100

    load query and gallery images correctly

--- train_ctl.py
+++ train_ctl.py
@@ -41,66 +41,82 @@ class ArmBenchDataset(Dataset):
         # get images ids for train or test
         train_test_split_data = pickle.load(open(os.path.join(training_dir, 'train-test-split.pickle'), 'rb'))
         if self.mode == 'train':
-            self.picks_ids = train_test_split_data['trainset']
-            self.objects_ids = train_test_split_data['trainset-objects']
+            self.query_objects_ids = train_test_split_data['trainset']
+            self.gallery_objects_list = train_test_split_data['trainset-objects']
         elif self.mode == 'test':
-            self.picks_ids = train_test_split_data['testset']
-            self.objects_ids = train_test_split_data['testset-objects']
+            self.query_objects_ids = train_test_split_data['testset']
+            self.gallery_objects_list = train_test_split_data['testset-objects']
 
         # load object images
         pixel_mean = [0.485, 0.456, 0.406]
         pixel_std = [0.229, 0.224, 0.225]
-        self.transforms = T.Compose([T.Normalize(mean=pixel_mean, std=pixel_std)])
+        image_size = [256, 256]
+        self.transforms = T.Compose([
+            T.Normalize(mean=pixel_mean, std=pixel_std),
+            T.Resize(image_size)
+        ])
 
     def __len__(self):
-        return len(self.picks_ids)  # number of picks in train or test
+        return len(self.query_objects_ids)  # number of query (picks) in train or test
 
     def load_folder_images(self, folder_path):
         images = []
         for img_path in glob.glob(folder_path + '/*.jpg'):
-            images.append(read_image(img_path).clone().float())
-        images = self.transforms(images)
+            images.append(self.transforms(read_image(img_path).clone().float()))
+        if len(images) == 0:
+            return []
+        # convert images list to tensor
+        images = torch.stack(images)
         return images
     
-    def load_pick_images(self, pick_id, query=False):
-        obj_id = self.objects_ids[pick_id]
-        gallery_imgs_paths = os.path.join(self.gallery_imgs_paths, obj_id)
+    def load_sample_images(self, query_id, query=False):
+        # read annotation.json
+        annotation_json = json.load(open(os.path.join(self.query_imgs_paths, self.query_objects_ids[query_id], 'annotation.json'), 'r'))
+        # get object id of pick_id from annotation.json
+        gallery_obj_id = annotation_json['GT_ID']
+        gallery_imgs_paths = os.path.join(self.gallery_imgs_paths, gallery_obj_id)
         gallery_imgs = self.load_folder_images(gallery_imgs_paths)
 
         if not query:
-            return gallery_imgs
+            return gallery_imgs, gallery_obj_id
         if query:
-            query_imgs_paths = os.path.join(self.query_imgs_paths, self.picks_ids[pick_id])
+            query_imgs_paths = os.path.join(self.query_imgs_paths, self.query_objects_ids[query_id])
             query_imgs = self.load_folder_images(query_imgs_paths)
 
-            return gallery_imgs, query_imgs
+            return query_imgs, gallery_imgs, gallery_obj_id
 
-    def __getitem__(self, pick_id):
+    def __getitem__(self, query_id):
         if self.mode == 'train':
-            anchor_imgs, positive_imgs = self.load_pick_images(pick_id)
-            positive_objs_id = self.objects_ids[pick_id]
+            anchor_imgs, positive_imgs, positive_obj_id = self.load_sample_images(query_id, query=True)
             # choose a random id from obj_ids as negative, make sure negative_obj_id is not the same as positive_obj_id
             while True:
-                negative_obj_id = random.randint(0, len(self.objects_ids) - 1)
-                if negative_obj_id != positive_objs_id:
+                negative_obj_id = random.randint(0, len(self.gallery_objects_list) - 1)
+                negative_obj_id = self.gallery_objects_list[negative_obj_id]
+                if negative_obj_id != positive_obj_id:
                     break
-            negative_imgs = self.load_pick_images(negative_obj_id, query=False)
+            negative_imgs, _ = self.load_sample_images(negative_obj_id, query=False)
             return anchor_imgs, positive_imgs, negative_imgs
         elif self.mode == 'test':
-            # load gallery images
-            gallery_imgs = self.load_pick_images(pick_id, query=False)
-            gallery_obj_id = self.objects_ids[pick_id]
+            # load query images
+            query_imgs, gallery_imgs, gallery_matching_id = self.load_sample_images(query_id, query=True)
             # get object id of all object in pick_id from container.json
-            container_json = json.load(open(os.path.join(self.query_imgs_paths, self.picks_ids[pick_id], 'container.json'), 'r'))
+            container_json = json.load(open(os.path.join(self.query_imgs_paths, self.query_objects_ids[query_id], 'container.json'), 'r'))
             container_objs_ids = container_json.keys()
             # load all images of all objects in pick_id
-            query_imgs = []
-            query_objs_ids = []
+            gallery_objs_imgs = [gallery_imgs]  # first object in the gallery is the matching query object
+            gallery_objs_ids = [gallery_matching_id]
             for obj_id in container_objs_ids:
-                query_imgs_paths = os.path.join(self.query_imgs_paths, self.picks_ids[pick_id], obj_id)
-                query_imgs.append(self.load_folder_images(query_imgs_paths))
-                query_objs_ids.append(obj_id)
-            return gallery_imgs, query_imgs, query_objs_ids, gallery_obj_id
+                if obj_id == gallery_matching_id:
+                    continue
+                gallery_imgs_paths = os.path.join(self.gallery_imgs_paths, obj_id)
+                this_obj_imgs = self.load_folder_images(gallery_imgs_paths)
+                # check if this_obj_imgs is empty
+                # TODO check if this is the same for the evaluation in the ArmBench paper
+                if len(this_obj_imgs) == 0:
+                    continue
+                gallery_objs_imgs.append(this_obj_imgs)
+                gallery_objs_ids.append(obj_id)
+            return query_imgs, gallery_objs_imgs, gallery_objs_ids, gallery_matching_id
 
 
 class CentroidTripletLoss(nn.Module):
@@ -157,30 +173,32 @@ def test_armbench(model, device, test_loader, epoch):
     rank_5 = []
 
     with torch.no_grad():
-        for pick_id in range(len(test_loader.dataset)):
+        for query_id in range(len(test_loader.dataset)):
             # get query and gallery images
-            gallery_images, query_objs_images, query_objs_ids, gallery_obj_id = test_loader.dataset[pick_id]
-            gallery_images, query_objs_images = gallery_images.to(device), [query_images.to(device) for query_images in query_objs_images]
+            query_imgs, gallery_objs_imgs, gallery_objs_ids, gallery_matching_id = test_loader.dataset[query_id]
+            query_imgs, gallery_objs_imgs = query_imgs.to(device), [gallery_imgs.to(device) for gallery_imgs in gallery_objs_imgs]
             # get embeddings
-            gallery_embeddings = model(gallery_images)
-            query_objs_embeddings = [model(query_images) for query_images in query_objs_images]
+            query_embeddings = model(query_imgs)
+            gallery_obj_embeddings = [model(gallery_imgs) for gallery_imgs in gallery_objs_imgs]
 
             # calculate centroids
-            gallery_centroid = torch.mean(gallery_embeddings, dim=0)
-            query_objs_centroid = [torch.mean(query_embeddings, dim=0) for query_embeddings in query_objs_embeddings]
+            query_centroid = torch.mean(query_embeddings, dim=0)
+            gallery_objs_centroids = [torch.mean(gallery_embeddings, dim=0) for gallery_embeddings in gallery_obj_embeddings]
 
             # calculate distances between gallery centroid and query objects centroids
-            distances = [torch.dist(gallery_centroid, query_objs_centroid[i]) for i in range(len(query_objs_centroid))]
+            distances = [torch.dist(query_centroid, gallery_centroid).cpu().numpy() for gallery_centroid in gallery_objs_centroids]
 
             # get top 5 closest objects
-            top_5 = np.argsort(distances)[:5]
+            top_5 = np.argsort(distances)[:5]  # TODO handle cases where there is less than 5 objects in gallery
+            # get object ids of top 5 closest objects
+            top_5 = [gallery_objs_ids[i] for i in top_5]
             # check if gallery_obj_id is in top_5
-            if gallery_obj_id in top_5:
+            if gallery_matching_id in top_5:
                 rank_5.append(1)
             else:
                 rank_5.append(0)
             # check if gallery_obj_id is the closest
-            if gallery_obj_id == top_5[0]:
+            if gallery_matching_id == top_5[0]:
                 rank_1.append(1)
             else:
                 rank_1.append(0)
@@ -195,7 +213,7 @@ def test_armbench(model, device, test_loader, epoch):
 def main():
     # Training settings
     parser = argparse.ArgumentParser(description='Train backbones for object classification (reidentification) using centroid loss using ArmBench dataset')
-    parser.add_argument('--training-batch-size', type=int, default=64, metavar='N',
+    parser.add_argument('--train-batch-size', type=int, default=64, metavar='N',
                         help='input batch size for training (= number of objects used in one training step) (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=500, metavar='N',
                         help='input batch size for testing (= number of objects used in one testing step) (default: 1000)')
@@ -207,14 +225,14 @@ def main():
                         help='For Saving the log and the models')
     args = parser.parse_args()
 
-    armbench_dataset_path = '/media/gouda/ssd_data/datasets/ArmBench'
+    armbench_dataset_path = '/media/gouda/ssd_data/datasets/armbench-object-id-0.1'
     output_path = '/home/gouda/segmentation/ctl_training_output/gouda/train_1'
     
     torch.manual_seed(args.seed)
 
     device = torch.device("cuda")
 
-    train_kwargs = {'batch_size': args.batch_size}
+    train_kwargs = {'batch_size': args.train_batch_size}
     test_kwargs = {'batch_size': args.test_batch_size}
     cuda_kwargs = {'num_workers': 4,
                    'pin_memory': True,
